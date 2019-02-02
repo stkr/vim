dd09dff8be07e5246346ebddbacf8cc9333c951d
stkr <stkr@users.noreply.github.com>
2019-02-02 10:30:27 +0000

    Add tmux config.

--- create-links.sh
+++ create-links.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DOTFILES=".vim .minttyrc"
+DOTFILES=".vim .minttyrc .tmux.conf"
 
 for F in $DOTFILES; do
     if [[ -e "$HOME/$F" ]]; then
