a2e6ed286295d8622977f22a8198f2f4d1dbbcb5
Stefan Krug <stkr@users.noreply.github.com>
2021-03-23 17:31:13 +0100

    Add neovim plugin for lsp-config.

---
+++ .local/share/nvim/site/pack/skr/start/nvim-lspconfig/lua/lspconfig.lua
@@ -0,0 +1,39 @@
+local configs = require 'lspconfig/configs'
+local lspinfo = require 'lspconfig/lspinfo'
+
+local M = {
+  util = require 'lspconfig/util';
+}
+
+M._root = {}
+
+function M.available_servers()
+  return vim.tbl_keys(configs)
+end
+
+-- Called from plugin/lspconfig.vim because it requires knowing that the last
+-- script in scriptnames to be executed is lspconfig.
+function M._root._setup()
+  M._root.commands = {
+    LspInfo = {
+      function()
+        lspinfo()
+      end;
+      "-nargs=?";
+      description = '`:LspInfo` Displays attached, active, and configured language servers';
+    };
+  };
+
+  M.util.create_module_commands("_root", M._root.commands)
+end
+
+local mt = {}
+function mt:__index(k)
+  if configs[k] == nil then
+    pcall(require, 'lspconfig/'..k)
+  end
+  return configs[k]
+end
+
+return setmetatable(M, mt)
+-- vim:et ts=2 sw=2
